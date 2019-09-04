9704a4f52b99401f5de2d5e30edfaf9b0ffab0a7
takato <ltakato@hotmail.com>
2019-09-04 13:14:25 -0300

    feat: add likes route and process image before storing it

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,3 +1,6 @@
+const sharp = require('sharp');
+const path = require('path');
+
 const Post = require('../models/Post');
 
 module.exports = {
@@ -11,12 +14,22 @@ module.exports = {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
 
+    const [name] = image.split('.');
+    const fileName = `${name}.jpg`;
+
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(
+        path.resolve(req.file.destination, 'resized', fileName)
+      );
+
     const post = await Post.create({
       author,
       place,
       description,
       hashtags,
-      image
+      image: fileName
     });
 
     return res.json(post);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://takato:takato@cluster0-djyr0.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
